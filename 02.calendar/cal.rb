8da4fa79062290032553a00f6d882cb891985834
hiroshima820 <53898556+hiroshima820@users.noreply.github.com>
2021-05-23 23:07:34 +0900

    %を表示しないように改行の表示を追加

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -23,9 +23,10 @@ print spaces_before_first_day
 dates.each do |date|
   if date == today
     spaces_before_today = date.day < 10 ? ' ' * 2 : ' '
-    print spaces_before_today + Paint[today.day, :inverse].to_s
+    print spaces_before_today + Paint[today.day, :inverse].to_s # Paint[foo, :inverse]でfooが反転して表示される
   else
     print date.day.to_s.rjust(3)
   end
   print "\n" if date.saturday?
 end
+print "\n" # 改行を付加して%を非表示
