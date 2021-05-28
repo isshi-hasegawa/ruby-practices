65292b2d2c52bc62e7f5549e661d21a6b016a65f
hiroshima820 <53898556+hiroshima820@users.noreply.github.com>
2021-05-29 08:55:33 +0900

    フレームごとに分割されたスコアを格納する処理を修正

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -3,34 +3,18 @@
 
 scores = ARGV[0].split(',')
 shots = []
-scores.each do |s|
-  if s == 'X'
+scores.each do |score|
+  if score == 'X'
     shots << 10
     shots << 0
   else
-    shots << s.to_i
+    shots << score.to_i
   end
 end
 
 frames = []
-shots[0..17].each_slice(2) do |s|
-  # 1~9フレーム目までを[[1, 2],[3, 4],[10, 0]..]のようにframesに格納
-  frames << s
-end
-
-if shots[18] == 10 && shots[20] == 10 && shots[22] == 10 # 10フレーム目がターキー
-  3.times { frames << [10, 0] }
-elsif shots[18] == 10 && shots[20] == 10 # 10フレーム目がダブル
-  2.times { frames << [10, 0] }
-  frames << [shots[22], 0]
-elsif shots[18] == 10 # 10フレーム目がストライク
-  frames << [10, 0]
-  frames << [shots[20], shots[21]]
-elsif shots[18] + shots[19] == 10 # 10フレーム目の2投目がスペアの場合
-  frames << [shots[18], shots[19]]
-  frames << [shots[20], 0]
-else
-  frames << [shots[18], shots[19]]
+shots.each_slice(2) do |shot|
+  frames << shot
 end
 
 point = 0
