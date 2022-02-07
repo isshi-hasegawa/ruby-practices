d9f6d384508bd4adc1ad50e11be4de70b056b2d4
Masashi Hasegawa <53898556+hiroshima820@users.noreply.github.com>
2022-02-07 09:40:35 +0900

    Shotクラスを定義

--- 07.bowling_object/shot.rb
+++ 07.bowling_object/shot.rb
@@ -1,1 +1,17 @@
+# frozen_string_literal: true
 
+class Shot
+  attr_reader :score
+
+  def initialize(mark)
+    @score = parse_mark(mark)
+  end
+
+  private
+
+  def parse_mark(mark)
+    return 10 if mark == 'X'
+
+    mark.to_i
+  end
+end
