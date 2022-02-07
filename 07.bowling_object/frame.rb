ef45b977ac36866573ee4c9c6c97c8ac8061bbcc
Masashi Hasegawa <53898556+hiroshima820@users.noreply.github.com>
2022-02-07 10:02:12 +0900

    Frameクラスを定義

--- 07.bowling_object/frame.rb
+++ 07.bowling_object/frame.rb
@@ -1,1 +1,30 @@
+# frozen_string_literal: true
 
+class Frame
+  attr_reader :frame_score
+
+  def initialize(first_mark, second_mark, third_mark = nil)
+    @first_shot = Shot.new(first_mark)
+    @second_shot = Shot.new(second_mark)
+    @third_shot = Shot.new(third_mark)
+    @frame_score = calc_frame_score
+  end
+
+  private
+
+  attr_reader :first_shot, :second_shot, :third_shot
+
+  def strike?
+    first_shot.score == 10
+  end
+
+  def spare?
+    [first_shot, second_shot].map(&:score).sum == 10
+  end
+
+  def calc_frame_score
+    shots = [first_shot, second_shot]
+    shots << third_shot if strike? || spare?
+    shots.map(&:score).sum
+  end
+end
