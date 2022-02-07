b75c3c94409308990c2507141e07d83d09bd6d37
Masashi Hasegawa <53898556+hiroshima820@users.noreply.github.com>
2022-02-07 14:53:02 +0900

    Gameクラスを定義

--- 07.bowling_object/game.rb
+++ 07.bowling_object/game.rb
@@ -1,1 +1,31 @@
+# frozen_string_literal: true
 
+class Game
+  FRAME_NUMBER = 10
+
+  def initialize(marks)
+    @marks = marks.split(',')
+  end
+
+  def score
+    frames = create_frames
+    frames.map(&:frame_score).sum
+  end
+
+  private
+
+  attr_reader :marks
+
+  def create_frames
+    frames = []
+    FRAME_NUMBER.times do
+      frames << Frame.new(*marks[0..2])
+      if marks[0] == 'X'
+        marks.shift(1)
+      else
+        marks.shift(2)
+      end
+    end
+    frames
+  end
+end
