2477b6c168500ac9267058dc14325e646f524f21
Masashi Hasegawa <53898556+hiroshima820@users.noreply.github.com>
2022-03-01 12:06:34 +0900

    Fileクラスのインスタンスメソッドが返却する値をフォーマットしないよう修正

--- 08.ls_object/lib/ls/file.rb
+++ 08.ls_object/lib/ls/file.rb
@@ -5,45 +5,27 @@ require 'etc'
 
 module Ls
   class File
-    FILETYPES = {
-      '01' => 'p',
-      '02' => 'c',
-      '04' => 'd',
-      '06' => 'b',
-      '10' => '-',
-      '12' => 'l',
-      '14' => 's'
-    }.freeze
-
-    PERMISSIONS = {
-      '0' => '---',
-      '1' => '--x',
-      '2' => '-w-',
-      '3' => '-wx',
-      '4' => 'r--',
-      '5' => 'r-x',
-      '6' => 'rw-',
-      '7' => 'rwx'
-    }.freeze
-
-    attr_reader :stat
-    private attr_reader :path, :mtime # rubocop:disable Style/AccessModifierDeclarations
+    private attr_reader :path, :stat # rubocop:disable Style/AccessModifierDeclarations
 
     def initialize(path)
       @path = path
       @stat = ::File.lstat(@path)
-      @mtime = @stat.mtime
     end
 
-    def filetype_and_permissions
-      mode_num = stat.mode.to_s(8).rjust(6, '0')
-      file_type = mode_num[0..1].gsub(/\d{2}/, FILETYPES)
-      permissions = mode_num[-3..].gsub(/\d/, PERMISSIONS)
-      file_type + permissions
+    def blocks
+      stat.blocks
+    end
+
+    def file_type
+      stat.ftype
+    end
+
+    def permission_numbers
+      stat.mode.to_s(8)[-3..]
     end
 
     def hard_link_size
-      stat.nlink.to_s
+      stat.nlink
     end
 
     def owner_name
@@ -55,22 +37,15 @@ module Ls
     end
 
     def byte_size
-      stat.size.to_s
+      stat.size
     end
 
     def modified_time
-      over_six_months_ago? ? mtime.strftime('%-m %e  %Y') : mtime.strftime('%-m %e %H:%M')
+      stat.mtime
     end
 
     def basename
       ::File.basename(path)
-    end
-
-    private
-
-    def over_six_months_ago?
-      six_months_ago = Date.today << 6
-      (six_months_ago <=> Date.parse(mtime.to_s)) == 1
     end
   end
 end
