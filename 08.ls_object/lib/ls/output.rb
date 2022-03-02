e4738f3992ea5252cde3a4e5cb6c2f2f6d249f68
Masashi Hasegawa <53898556+hiroshima820@users.noreply.github.com>
2022-03-02 23:53:59 +0900

    Fileクラス修正に伴うoutput_long_formatの修正

--- 08.ls_object/lib/ls/output.rb
+++ 08.ls_object/lib/ls/output.rb
@@ -7,11 +7,32 @@ module Ls
   class Output
     COLUMN = 3
 
+    FILETYPES = {
+      'fifo' => 'p',
+      'characterSpecial' => 'c',
+      'directory' => 'd',
+      'blockSpecial' => 'b',
+      'file' => '-',
+      'link' => 'l',
+      'socket' => 's'
+    }.freeze
+
+    PERMISSIONS = {
+      '0' => '---',
+      '1' => '--x',
+      '2' => '-w-',
+      '3' => '-wx',
+      '4' => 'r--',
+      '5' => 'r-x',
+      '6' => 'rw-',
+      '7' => 'rwx'
+    }.freeze
+
     private attr_reader :args, :files # rubocop:disable Style/AccessModifierDeclarations
 
     def initialize
       @args = Ls::Args.new
-      @files = file_paths.map { |path| Ls::File.new(path) }
+      @files = collect_file_paths.map { |path| Ls::File.new(path) }
     end
 
     def run
@@ -20,7 +41,7 @@ module Ls
 
     private
 
-    def file_paths
+    def collect_file_paths
       pattern = args.pathname
       params = args.all? ? [pattern, ::File::FNM_DOTMATCH] : [pattern]
       file_paths = Dir.glob(*params).sort
@@ -28,29 +49,46 @@ module Ls
     end
 
     def total_blocks
-      "total #{files.map(&:stat).map(&:blocks).sum}"
+      "total #{files.map(&:blocks).sum}"
     end
 
-    def find_max_column_length(column)
-      files.map(&column).max_by(&:length).length
+    def build_data(file)
+      {
+        type_and_mode: FILETYPES[file.file_type] + file.permission_number.gsub(/\d/, PERMISSIONS),
+        nlink: file.hard_link_size.to_s,
+        user: file.owner_name,
+        group: file.group_name,
+        size: file.byte_size.to_s,
+        mtime: file.modified_time.strftime('%b %e %H:%M'),
+        basename: file.basename
+      }
     end
 
-    def format_row(file, column, additional_space = 0, direction = :rjust)
-      width = find_max_column_length(column) + additional_space
-      file.send(column).to_s.send(direction, width)
+    def find_max_length(column)
+      files.map { |file| build_data(file)[column] }
+           .max_by(&:length)
+           .length
+    end
+
+    def format_row(data, max_nlink, max_user, max_group, max_size)
+      [
+        data[:type_and_mode],
+        data[:nlink].rjust(max_nlink + 1),
+        data[:user].ljust(max_user + 1),
+        data[:group].ljust(max_group + 1),
+        data[:size].rjust(max_size),
+        data[:mtime],
+        data[:basename]
+      ].join(' ')
     end
 
     def rows
+      max_lengths = %i[nlink user group size].map do |key|
+        find_max_length(key)
+      end
       files.map do |file|
-        [
-          format_row(file, :filetype_and_permissions),
-          format_row(file, :hard_link_size),
-          format_row(file, :owner_name, 0, :ljust),
-          format_row(file, :group_name, 1),
-          format_row(file, :byte_size, 1),
-          format_row(file, :modified_time),
-          format_row(file, :basename, 0, :ljust)
-        ].join(' ')
+        data = build_data(file)
+        format_row(data, *max_lengths)
       end
     end
 
@@ -63,10 +101,10 @@ module Ls
     end
 
     def output
-      file_paths = files.map { |file| format_row(file, :basename, 0, :ljust) }
-      (COLUMN - remainder).times { file_paths << [''] } unless remainder.zero?
-      row_count = file_paths.length / COLUMN
-      file_paths.each_slice(row_count).to_a.transpose.map { |row| row.join("\t") }
+      file_names = files.map { |file| format_row(file) }
+      (COLUMN - remainder).times { file_names << [''] } unless remainder.zero?
+      row_count = file_names.length / COLUMN
+      file_names.each_slice(row_count).to_a.transpose.map { |row| row.join("\t") }
     end
   end
 end
